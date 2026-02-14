070882a7be9907323ff64ebde27de3fcee3ee1dc
Elijah Alford <ejalford@uncg.edu>
2026-02-13 19:52:35 -0500

    Remove duplicate password.update route

--- resources/js/pages/settings/profile.tsx
+++ resources/js/pages/settings/profile.tsx
@@ -1,4 +1,4 @@
-import ProfileController from '@/actions/App/Http/Controllers/Settings/ProfileController';
+//import ProfileController from '@/actions/App/Http/Controllers/Settings/ProfileController';
 import { send } from '@/routes/verification';
 import { type BreadcrumbItem, type SharedData } from '@/types';
 import { Transition } from '@headlessui/react';
@@ -12,15 +12,15 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import AppLayout from '@/layouts/app-layout';
 import SettingsLayout from '@/layouts/settings/layout';
-import { edit } from '@/routes/profile';
+//import { edit } from '@/routes/profile';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Profile settings',
-        href: edit().url,
+        href: '/settings/profile', 
     },
 ];
-
+ 
 export default function Profile({
     mustVerifyEmail,
     status,
